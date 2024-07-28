e333f85e134e00463badf1f28567dbd2050a3423
Taharka2000 <sammbasow1999@gmail.com>
2024-07-28 17:36:04 +0000

    Initial commit of React project

--- terrorfm/deploy.sh
+++ terrorfm/deploy.sh
@@ -3,12 +3,26 @@
 cd /home/sammba/Bureau/bonjour
 
 
-     if [ ! -d ".git" ]; then
-       git init
-     fi
-     git remote remove origin 2>/dev/null || true
-     git remote add origin https://github.com/Taharka2000/terraformsam.git
-     git add .
-     git commit -m "Initial commit of React project" || echo "No changes to commit."
-     git branch -M main
-     git push -u origin main
+     #Initialize a new Git repository if one doesn't exist
+if [ ! -d ".git" ]; then
+  git init
+fi
+
+# Check if the remote 'origin' exists
+git remote | grep -q 'origin'
+if [ $? -ne 0 ]; then
+  git remote add origin https://github.com/Taharka2000/terraformsam.git
+fi
+
+# Pull the latest changes from the remote repository
+git pull origin main
+
+# Add all changes to the staging area, excluding terraform.tfvars
+git add --all
+git reset terraform.tfvars
+
+# Commit the changes
+git commit -m "Initial commit of React project"
+
+# Push the changes to the remote repository
+    git push -u origin main
